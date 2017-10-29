1d72cc3f2b464f6d51e8d1acd7626bf6cef7b6d1
fintzij <fintzij@uw.edu>
2017-10-29 00:43:57 -0700

    so much ugh

--- src/map_draws_2_lna.cpp
+++ src/map_draws_2_lna.cpp
@@ -124,7 +124,7 @@ void map_draws_2_lna(arma::mat& pathmat,
                         if(!good_svd) {
                                 throw std::runtime_error("SVD failed.");
                         } else {
-                                svd_d.elem(arma::find(svd_d < sqrt(arma::datum::eps))).zeros();     // zero out negative singular values
+                                svd_d.elem(arma::find(svd_d <= sqrt(arma::datum::eps))).zeros(); // zero out negative singular values
                                 svd_V.each_row() %= arma::sqrt(svd_d).t();     // multiply rows of V by sqrt of singular vals
                                 log_lna = lna_drift + (svd_U * svd_V.t()) * draws.col(j); // map the LNA draws
                         }
@@ -152,6 +152,13 @@ void map_draws_2_lna(arma::mat& pathmat,
                 // update the initial volumes
                 init_volumes += stoich_matrix * nat_lna;
 
+                // cout << "j = " << j << endl;
+                // cout << "lna_drift: " << lna_drift << endl;
+                // cout << "lna_diffusion: " << lna_diffusion << endl;
+                // cout << "lna_sqrt_diff: " << (svd_U * svd_V.t()) << endl;
+                // cout << "nat_lna: " << nat_lna << endl;
+                // cout << "init_volumes: " << init_volumes << endl;
+
                 // apply forcings if called for - applied after censusing the path
                 if(forcing_inds[j+1]) {
                         init_volumes += forcing_matrix.col(j+1);
@@ -159,8 +166,12 @@ void map_draws_2_lna(arma::mat& pathmat,
 
                 // if any increments or volumes are negative, throw an error
                 try{
-                        if(any(nat_lna < 0) || any(init_volumes < 0)) {
-                                throw std::runtime_error("Negative increment or compartment volumes.");
+                        if(any(nat_lna < 0)) {
+                                throw std::runtime_error("Negative increment.");
+                        }
+
+                        if(any(init_volumes < 0)) {
+                                throw std::runtime_error("Negative compartment volumes.");
                         }
 
                 } catch(std::runtime_error &err) {
